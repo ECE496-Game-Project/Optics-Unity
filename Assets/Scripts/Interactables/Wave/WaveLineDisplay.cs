81e3d925b20144fd28a34074542cb9c68eb0b5a6
Yang Chen <jackchen.chen@mail.utoronto.ca>
2023-06-30 09:56:25 -0400

    final fix, merge back to dev after

--- Assets/Scripts/Interactables/Wave/WaveLineDisplay.cs
+++ Assets/Scripts/Interactables/Wave/WaveLineDisplay.cs
@@ -9,21 +9,23 @@ namespace GO_Wave {
         public int m_SampleCount;
         [SerializeField] private float _perSampleSpaceLength;
         [SerializeField] private GameObject _samplePointPrefab;
-        private Transform[] _samplePointList;
+        private WaveLineSample[] _samplePointList;
 
         [Range(0.0f, 5.0f)]
         [SerializeField] private float _timeScale = 1.0f;
 
-        public float TimeScaleGetter() {
-            return _timeScale;
-        }
+        private WaveSource _activeWS;
 
-        void Start() {
-            if(_samplePointPrefab.GetComponent<WaveLineSample>() == null) {
+        private void Start() {
+            _activeWS = GetComponent<WaveSource>();
+            if (_activeWS == null) {
+                DebugLogger.Error(this.name, "GameObject Doesn't contains WaveSource Script, Stop Executing.");
+            }
+            if (_samplePointPrefab.GetComponent<WaveLineSample>() == null) {
                 DebugLogger.Error(this.name, "Prefab Doesn't contains WaveLineSample Script, Stop Executing.");
             }
 
-            _samplePointList = new Transform[m_SampleCount];
+            _samplePointList = new WaveLineSample[m_SampleCount];
             for (int i = 0; i < m_SampleCount; i++) {
                 Vector3 initPos = this.transform.position + i * _perSampleSpaceLength * this.transform.forward;
                 _samplePointList[i] = Object.Instantiate(
@@ -31,14 +33,15 @@ namespace GO_Wave {
                     initPos,
                     Quaternion.LookRotation(this.transform.forward, this.transform.up),
                     this.transform
-                ).transform;
+                ).GetComponent<WaveLineSample>();
+            }
+        }
 
-                /*Init LineWaveSample*/
-                _samplePointList[i].GetComponent<WaveLineSample>().init(
-                    this.name + "_Sample[" + i + "]",
-                    this.GetComponent<WaveSource>().ActiveWaveParams,
-                    TimeScaleGetter
-                );
+        private void Update() {
+            for (int i = 0; i < m_SampleCount; i++) {
+                Vector3 vec = WaveAlgorithm.CalcIrradiance(_samplePointList[i].transform.position - this.transform.position, Time.time * _timeScale, _activeWS.ActiveWaveParams);
+
+                _samplePointList[i].UpdateEField(vec);
             }
         }
     }
--- Assets/Scripts/Interactables/Wave/WaveLineSample.cs
+++ Assets/Scripts/Interactables/Wave/WaveLineSample.cs
@@ -6,19 +6,7 @@ using CommonUtils;
 namespace GO_Wave {
     public class WaveLineSample : MonoBehaviour {
         
-        [HideInInspector] public WaveParams EffectiveWaveParam;
-        public del_floatGetter TimeScale;
-
-        public void init(string name, WaveParams wp, del_floatGetter tsGetter) {
-            this.name = name;
-            EffectiveWaveParam = wp;
-            TimeScale = tsGetter;
-        }
-
-        public void FixedUpdate() {
-            if (EffectiveWaveParam == null) return;
-            Vector3 vec = WaveAlgorithm.CalcIrradiance(this.transform.position - EffectiveWaveParam.Origin(), Time.time * TimeScale(), EffectiveWaveParam);
-
+        public void UpdateEField(Vector3 vec) {
             this.transform.LookAt((this.transform.position + this.transform.forward), vec);
             this.transform.localScale = new Vector3(1, vec.magnitude, 1);
         }
