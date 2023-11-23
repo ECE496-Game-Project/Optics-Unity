18e01aa3ac993c216a4d39bb0badf8d4148d416b
Yang Chen <jackchen.chen@mail.utoronto.ca>
2023-11-22 21:35:08 -0500

    git working on SelectParamView

--- Assets/Scripts/Interactables/Devices/DeviceBases.cs
+++ Assets/Scripts/Interactables/Devices/DeviceBases.cs
@@ -14,8 +14,13 @@ namespace GO_Device {
         public DEVICETYPE DeviceType = DEVICETYPE.INVALID;
         public virtual void WaveHit(in RaycastHit hit, WaveSource parentWS) { }
         public virtual void CleanDeviceHitTrace(WaveSource parentWS) { }
-        
-        public virtual void RegisterParametersCallback(ParameterInfoList ParameterInfos) { }
+
+        public virtual void RegisterParametersCallback(ParameterInfoList ParameterInfos) {
+            var RotDegTuple = (ParameterInfo<int>)ParameterInfos.SymbolQuickAccess["DeviceType"];
+            RotDegTuple.Getter = () => { return (int)DeviceType; };
+            RotDegTuple.Default = (int)DeviceType;
+            RotDegTuple.Setter = (evt) => { DeviceType = (DEVICETYPE)evt.newValue; ParameterChangeTrigger(); };
+        }
         public virtual void ParameterChangeTrigger() { }
     }
 }
