9fa5c707ec230f0a3c6dff747b6cbf045266cbaf
Yang Chen <jackchen.chen@mail.utoronto.ca>
2024-01-02 13:44:13 -0500

    in progress

--- Assets/Scripts/Frontend/DeviceOrderPanel.cs
+++ Assets/Scripts/Frontend/DeviceOrderPanel.cs
@@ -17,6 +17,10 @@ namespace Panel {
 
         [SerializeField] private int SlotNum = 8;
 
+        bool isDragging = false;
+        Vector2 startMousePosition;
+        Vector2 startImagePosition;
+
         private void Awake() {
             _img_list = new List<Image>();
         }
@@ -39,11 +43,60 @@ namespace Panel {
                 _img_list.Add(img);
             }
 
-            //WaveOrderManager.Instance.DeviceCount;
+        }
+
+        public void DraggableElement() {
+            var image = _img_list[0];
+
+            image.RegisterCallback<PointerDownEvent>(evt =>
+            {
+                isDragging = true;
+                image.style.position = Position.Absolute;
+                image.style.left = evt.position.x;
+                image.style.top = evt.position.y;
+
+            });
+
+            image.RegisterCallback<PointerMoveEvent>(evt =>
+            {
+                if (isDragging) {
+                    Debug.Log("Position: " + evt.position);
+                    Debug.Log("LocalPosition: " + evt.localPosition);
+                    Debug.Log("DeltaPosition: " + evt.deltaPosition);
+                    Debug.Log("\n\n");
+                    image.style.left = evt.position.x;
+                    image.style.top = evt.position.y;
+                }
+            });
+
+            image.RegisterCallback<PointerUpEvent>(evt =>
+            {
+                isDragging = false;
+            });
+        }
+
+        private void BeginDrag(PointerDownEvent evt) {
+            isDragging = true;
+            startMousePosition = evt.localPosition;
+            //startImagePosition = image.transform.position;
+            evt.StopPropagation();
+        }
+
+        private void Dragging(PointerMoveEvent evt) {
+            //evt.
+            if (evt.pressedButtons == 1) // Left mouse button (change to evt.button if available)
+            {
+                Debug.Log("Dragging");
+            }
+        }
+
+        private void EndDrag(PointerUpEvent evt) {
+            // Handle end of drag here
+            Debug.Log("EndDrag");
         }
 
         void Start() {
-
+            DraggableElement();
         }
 
         // Update is called once per frame
