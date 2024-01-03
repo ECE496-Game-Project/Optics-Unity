18b258eefc7b35a4ba8aefaf67f493d265b4c623
Yang Chen <jackchen.chen@mail.utoronto.ca>
2024-01-02 22:20:20 -0500

    working

--- Assets/Scripts/Frontend/DeviceOrderPanel.cs
+++ Assets/Scripts/Frontend/DeviceOrderPanel.cs
@@ -8,18 +8,31 @@ using CommonUtils;
 using Constraint;
 
 namespace Panel {
+    public class DeviceLabel : VisualElement {
+        Image DeviceIcon;
+
+        DeviceLabel(Texture img) {
+            DeviceIcon = new Image();
+            DeviceIcon.image = img;
+        }
+    }
+
     public class DeviceOrderPanel : MonoSingleton<DeviceOrderPanel>{
         [SerializeField] private UIDocument _uiDocument;
         [SerializeField] private StyleSheet _styleSheet;
 
         private VisualElement _root;
+        private VisualElement _bar;
         private List<Image> _img_list;
 
-        [SerializeField] private int SlotNum = 8;
+        /* Slot informations */
+        [SerializeField] private int MaxSlot = 1;
+        [SerializeField] private float Left = 20;
+        [SerializeField] private float Width = 60;
+        [SerializeField] private float Top = 90;
 
         bool isDragging = false;
-        Vector2 startMousePosition;
-        Vector2 startImagePosition;
+        Image DraggingTarget;
 
         private void Awake() {
             _img_list = new List<Image>();
@@ -30,77 +43,62 @@ namespace Panel {
         }
 
         void Generate() {
+
             if (_uiDocument == null || _styleSheet == null) return;
 
             _root = _uiDocument.rootVisualElement;
+
             _root.styleSheets.Add(_styleSheet);
             _root.AddToClassList("root");
 
-            for (int i = 0; i < SlotNum; i++) {
-                Image img = new Image();
-                img.AddToClassList("slot");
+            _bar = new VisualElement();
+            _bar.AddToClassList("progress-container");
+            _bar.style.left = new StyleLength(new Length(Left, LengthUnit.Percent));
+            _bar.style.width = new StyleLength(new Length(Width, LengthUnit.Percent));
+            _bar.style.top = new StyleLength(new Length(Top, LengthUnit.Percent));
+
+            _root.Add(_bar);
+
+            for (int i = 0; i < MaxSlot; i++) {
+                Image img = GenIcon();
+                
                 _root.Add(img);
                 _img_list.Add(img);
             }
-
         }
 
-        public void DraggableElement() {
-            var image = _img_list[0];
+        public Image GenIcon() {
+            Image img = new Image();
+            img.image = Resources.Load<Texture2D>("Art/Images/Polarizer");
+            img.AddToClassList("slot");
+            img.style.right = new StyleLength(new Length((100 - Left - Width), LengthUnit.Percent));
+            img.style.top = new StyleLength(new Length(Top, LengthUnit.Percent));
 
-            image.RegisterCallback<PointerDownEvent>(evt =>
-            {
+            img.RegisterCallback<PointerDownEvent>(evt => {
                 isDragging = true;
-                //image.style.position = Position.Absolute;
-                //image.style.left = evt.position.x;
-                //image.style.top = evt.position.y;
-
+                DraggingTarget = (Image)evt.currentTarget;
             });
 
-            image.RegisterCallback<PointerMoveEvent>(evt =>
-            {
-                if (isDragging) {
-                    Debug.Log("Position: " + evt.position);
-                    Debug.Log("LocalPosition: " + evt.localPosition);
-                    Debug.Log("DeltaPosition: " + evt.deltaPosition);
-                    Debug.Log("\n\n");
-                    //image.style.left = evt.position.x;
-                    //image.style.top = evt.position.y;
-                }
-            });
-
-            image.RegisterCallback<PointerUpEvent>(evt =>
-            {
+            img.RegisterCallback<PointerUpEvent>(evt => {
                 isDragging = false;
+                DraggingTarget = null;
             });
-        }
-
-        private void BeginDrag(PointerDownEvent evt) {
-            isDragging = true;
-            startMousePosition = evt.localPosition;
-            //startImagePosition = image.transform.position;
-            evt.StopPropagation();
-        }
-
-        private void Dragging(PointerMoveEvent evt) {
-            //evt.
-            if (evt.pressedButtons == 1) // Left mouse button (change to evt.button if available)
-            {
-                Debug.Log("Dragging");
-            }
-        }
-
-        private void EndDrag(PointerUpEvent evt) {
-            // Handle end of drag here
-            Debug.Log("EndDrag");
+            return img;
         }
 
         void Start() {
-            DraggableElement();
         }
 
         // Update is called once per frame
         void Update() {
+            if (isDragging) {
+                // Calculate the percentage of the mouse position relative to the screen width
+                float xPrec = (Screen.width - Input.mousePosition.x / Screen.width) * 100;
+
+                // Set the 'right' style property
+                DraggingTarget.style.right = new StyleLength(new Length(xPrec, LengthUnit.Percent));
+            }
+
 
         }
     }
