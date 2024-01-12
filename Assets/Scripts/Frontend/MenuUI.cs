08dc45031e98c66552a93a29f609752d1449691d
Chuyue Zhang <irondumpling233@outlook.com>
2024-01-12 11:59:56 -0500

    Add devices ui

---
+++ Assets/Scripts/Frontend/MenuUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUI : MonoBehaviour
+{
+    [SerializeField] private enum pageName {
+        Home = 1,
+        Free_Scene = 2,
+        Tutorial = 3
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
