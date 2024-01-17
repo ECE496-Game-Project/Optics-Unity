479631726560e5a9d677049159071647b0083463
Chuyue Zhang <irondumpling233@outlook.com>
2024-01-17 16:00:49 -0500

    Page Render Works

--- Assets/Scripts/Frontend/MenuUI.cs
+++ Assets/Scripts/Frontend/MenuUI.cs
@@ -8,6 +8,8 @@ using CommonUtils;
 
 public class MenuUI : MonoBehaviour
 {
+    #region Common Page
+
     public enum PAGENAME {
         Home = 1,
         Free_Scene = 2,
@@ -38,31 +40,89 @@ public class MenuUI : MonoBehaviour
         VisualElement root = doc.rootVisualElement;
         PreRegisterCallback(root);
 
-        if(page_name == PAGENAME.Home) RenderHome();
-        else if(page_name == PAGENAME.Free_Scene) RenderFreeScene();
-        else if(page_name == PAGENAME.Tutorial) RenderTutorial();
+        if(page_name == PAGENAME.Home) RenderHome(root);
+        else if(page_name == PAGENAME.Free_Scene) RenderFreeScene(root);
+        else if(page_name == PAGENAME.Tutorial) RenderTutorial(root);
     }
+
+    #endregion
 
     #region Home Page
 
-    public void RenderHome(){
-
+    public void RenderHome(VisualElement root){
+        VisualElement content = root.Q("content");
+        if(content == null){
+            Debug.LogWarning("Not found content");
+            return;
+        }
+        
+        var logoUI = Resources.Load<VisualTreeAsset>("Art/Frontend/PageUI/HomePage/LogoUI");
+        content.Add(logoUI.Instantiate());
     }
 
     #endregion
 
     #region Free Scene Page
 
-    public void RenderFreeScene(){
-        
+    public void RenderFreeScene(VisualElement root){
+        VisualElement content = root.Q("content");
+        if(content == null){
+            Debug.LogWarning("Not found content");
+            return;
+        }
+
+        var addSceneUI = Resources.Load<VisualTreeAsset>("Art/Frontend/PageUI/FreeScenePage/AddSceneButtonUI");
+        content.Add(addSceneUI.Instantiate());
+
+        // TODO: register an event, click add scene ui to create a new scene ui & create a new scene
     }
 
     #endregion
 
     #region Tutorial Page
+    
+    Dictionary<string, List<string>> tutDict = new Dictionary<string, List<string>>{
+        {"Wave", new List<string> {"Basics"}},
+        {"Polarization", new List<string> {"Polarizer", "Waveplate"}}
+    };
 
-    public void RenderTutorial(){
-        
+    public void RenderTutorial(VisualElement root){
+        VisualElement content = root.Q("content");
+        if(content == null){
+            Debug.LogWarning("Not found content");
+            return;
+        }
+
+        VisualElement header = root.Q("header");
+        if(header == null){
+            Debug.LogWarning("Not found header");
+            return;
+        }        
+
+        var sectionUI = Resources.Load<VisualTreeAsset>("Art/Frontend/PageUI/TutorialPage/SectionUI");
+        var sectionButton = Resources.Load<VisualTreeAsset>("Art/Frontend/PageUI/TutorialPage/SectionButtonUI");
+        var courseButton = Resources.Load<VisualTreeAsset>("Art/Frontend/PageUI/TutorialPage/CourseButtonUI");
+
+        foreach(var section in tutDict){
+            // header.Add(sectionButton.Instantiate());
+
+            VisualElement secUI = sectionUI.Instantiate();
+            Label sectionName = secUI.Q<Label>("name");
+            sectionName.text = section.Key;
+            content.Add(secUI);
+
+            VisualElement sectionContent = secUI.Q("sectionContent");
+            
+            foreach(var course in section.Value){
+                VisualElement crsButton = courseButton.Instantiate();
+                Label courseName = crsButton.Q<Label>("name");
+                courseName.text = course;
+                VisualElement courseThumbnail = crsButton.Q("Thumbnail");
+                courseThumbnail.style.backgroundImage = Resources.Load<Texture2D>($"Art/Images/Tutorial/{course}");
+                // TODO: Register event to load corresponding scene
+                sectionContent.Add(crsButton);
+            }
+         }
     }
 
     #endregion
