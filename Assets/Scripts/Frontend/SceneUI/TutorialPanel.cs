e49d0b7647f1b680dcc1f8f2ac622c6ea233fa9c
Chuyue Zhang <irondumpling233@outlook.com>
2024-03-24 22:17:26 -0400

    open tutorial

--- Assets/Scripts/Frontend/SceneUI/TutorialPanel.cs
+++ Assets/Scripts/Frontend/SceneUI/TutorialPanel.cs
@@ -9,12 +9,55 @@ public class TutorialPanel : MonoBehaviour
     public UIDocument doc;
     private const int PANEL_WIDTH = 30;
     private const float HIDE_POSITION = 98.5f;
-    bool isPanelExpanded = false;
+    bool isPanelExpanded = true;
     bool isPaused = false;
     int curPage = 1;
     int maxPage = 3;
+
+    public void SetPageDisplay(bool display, int pgn) {
+        VisualElement page = doc.rootVisualElement.Q<VisualElement>(name: ("page"+ pgn.ToString()));
+        page.style.display = display ? DisplayStyle.Flex : DisplayStyle.None;
+    }
+
+    void Awake()
+    {
+        VisualElement root = doc.rootVisualElement;
+        OpenExpandPanel(root);
+        PreRegisterCallback(root);
+    }
+
+    public void CloseExpandPanel(VisualElement root){
+        VisualElement expPanel = root.Q<VisualElement>(name: "ExpandPanel");
+        VisualElement expBody = root.Q<VisualElement>(name: "Body");
+        Button expButton = root.Q<Button>(name: "ExpandButton");
+
+        expPanel.style.width = PANEL_WIDTH;
+        Length width = new Length(HIDE_POSITION, LengthUnit.Percent);
+        expPanel.style.left = new StyleLength(width);
+        expBody.style.display = DisplayStyle.None;
+
+        expButton.text = "\u2190";
+        isPanelExpanded = false;
+    }
+
+    public void OpenExpandPanel(VisualElement root){
+        VisualElement expPanel = root.Q<VisualElement>(name: "ExpandPanel");
+        VisualElement expBody = root.Q<VisualElement>(name: "Body");
+        Button expButton = root.Q<Button>(name: "ExpandButton");
+
+        Length width = new Length(PANEL_WIDTH, LengthUnit.Percent);
+        expPanel.style.width = new StyleLength(width);
+        width = new Length(100 - PANEL_WIDTH, LengthUnit.Percent);
+        expPanel.style.left = new StyleLength(width);
+        expBody.style.display = DisplayStyle.Flex;
+
+        expButton.text = "\u2192";
+        isPanelExpanded = true;
+    }
+
     public void PreRegisterCallback(VisualElement root) {
         Button expButton = root.Q<Button>(name: "ExpandButton");
+
         expButton.clicked += () => {
             if(isPanelExpanded) CloseExpandPanel(root);
             else OpenExpandPanel(root);
@@ -48,45 +91,5 @@ public class TutorialPanel : MonoBehaviour
             }
             isPaused = !isPaused;
         };
-    }
-
-    public void SetPageDisplay(bool display, int pgn) {
-        VisualElement page = doc.rootVisualElement.Q<VisualElement>(name: ("page"+ pgn.ToString()));
-        page.style.display = display ? DisplayStyle.Flex : DisplayStyle.None;
-    }
-
-    void Awake()
-    {
-        VisualElement root = doc.rootVisualElement;
-        CloseExpandPanel(root);
-        PreRegisterCallback(root);
-    }
-
-    public void CloseExpandPanel(VisualElement root){
-        VisualElement expPanel = root.Q<VisualElement>(name: "ExpandPanel");
-        VisualElement expBody = root.Q<VisualElement>(name: "Body");
-        Button expButton = root.Q<Button>(name: "ExpandButton");
-
-        expPanel.style.width = PANEL_WIDTH;
-        Length width = new Length(HIDE_POSITION, LengthUnit.Percent);
-        expPanel.style.left = new StyleLength(width);
-        expBody.style.display = DisplayStyle.None;
-
-        expButton.text = "\u2190";
-        isPanelExpanded = false;
-    }
-    public void OpenExpandPanel(VisualElement root){
-        VisualElement expPanel = root.Q<VisualElement>(name: "ExpandPanel");
-        VisualElement expBody = root.Q<VisualElement>(name: "Body");
-        Button expButton = root.Q<Button>(name: "ExpandButton");
-
-        Length width = new Length(PANEL_WIDTH, LengthUnit.Percent);
-        expPanel.style.width = new StyleLength(width);
-        width = new Length(100 - PANEL_WIDTH, LengthUnit.Percent);
-        expPanel.style.left = new StyleLength(width);
-        expBody.style.display = DisplayStyle.Flex;
-
-        expButton.text = "\u2192";
-        isPanelExpanded = true;
     }
 }
